6713f895a229c1c29583f90294d70892ffc9f8af
PhillipAColes <phillipanthonycoles@gmail.com>
2020-05-21 23:16:32 +0100

    aesthetic changes

--- RovibEnerNN.m
+++ RovibEnerNN.m
@@ -27,11 +27,7 @@ X = [J K];
 %X = [data(:,10:15) data(:,9) data(:,4) data(:,7) JJ1 JJ2 JK K4 ];
 
 %%
-% clear all;  clc
-% cd C:\Users\Phillip\Workspace\ML\RovibEner
-%  data = load('backproptest-1');
-%  y = data(:,1);
-%  X = [data(:,2) data(:,3)];
+% clear all; clc; clf
 
 %%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%%
 %%%% user should modify the below %%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%%%%
@@ -49,11 +45,11 @@ activation_function_type = {'sigmoid', 'linear'};
 feature_scaling_tf = true;
 
 % regularisation parameter
-lambda = 0.0003
+lambda = 0.003
 
 %%%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%%%%
 
-%%% simple test cases performed with pen and paper ~%%
+%%% simple test cases ~%%
 %load('Test_5.mat')
 
 %%%%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%%
@@ -108,14 +104,14 @@ X = [ones(num_data_samples,1) X];
 [activation] = ForwardPropagation(weights_array, num_layers, num_data_samples, num_units, ...
                                    activation_function_type, X);
                                
-%fprintf('predictions for %d data points are: \n',num_data_samples);
 hypothesis = activation{num_layers};
 activation_error{num_layers-1} = hypothesis' - y';
 
 
 cost = ComputeCost(activation_error{num_layers-1}, weights_array, ...
                                 num_data_samples, num_layers, lambda)
-%%
+
+
 % Unroll weights ready for backpropagation
 nn_weights = [];               
 for i=1:num_layers-1
@@ -134,33 +130,30 @@ grad = Vec2CellArray(unrolled_grad,num_layers,num_units);
 numerical_grad = CalcNumericalGradient(weights_array, num_layers,...
                     num_data_samples, num_units, ...
                     activation_function_type, X, y, lambda); 
+                
 %%
+
 % Create shorthand for cost function to be minimised
 backPropagation = @(p) NNBackPropagation(p, X, y, num_layers, ...
                                 num_data_samples, num_units, ...
                                 activation_function_type, lambda);
                 
+
+
 %options = optimset('MaxIter', 1000);
-
-% Now, costFunction is a function that takes in only one argument (the
-% neural network parameters)
 %[nn_params, cost] = fmincg(backPropagation, nn_weights, options);
-
 options = optimoptions('fminunc','GradObj','on','Display','iter','MaxIter',200)
 [nn_params, cost] = fminunc(backPropagation, nn_weights, options);
 
-%%
+%% 
 
+% One final pass of forward propagation to get our final hypothesis
 weights_array = Vec2CellArray(nn_params,num_layers,num_units);
-
-%  x1 = linspace(min(X(:,2)),max(X(:,2)),1000)';
-%  x2 = linspace(min(X(:,3)),max(X(:,3)),1000)';  
-%  Xtest = [ones(num_data_samples,1) x1 x2];
 
 activation = ForwardPropagation(weights_array, num_layers,...
                                    num_data_samples, num_units, ...
                                    activation_function_type, X);                                      
-
+% And plot our results
 plot3(X(:,2),X(:,3),y,'o')
 hold on                                       
 plot3(X(:,2),X(:,3),activation{num_layers},'-')
